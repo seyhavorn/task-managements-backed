0b193bd3e574b400400a64f1bc77e6df7090ca24
seyhavorn <vornseyha4758@gmail.com>
2022-07-21 21:12:58 +0700

    -Create task Module , -taskController, - Create a service that has: getTask, getTaskById, PostTask, - Create Model

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,9 +1,24 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { TasksService } from './tasks.service';
+import { Task } from './task.model';
+import { CreateTaskDto } from './dto/create-task.dto';
 
 @Controller('tasks')
 export class TasksController {
+  constructor(private tasksService: TasksService) {}
+
   @Get()
-  findAll(): string {
-    return 'This is my tasks that I created before.';
+  getAllTasks(): Task[] {
+    return this.tasksService.getAllTasks();
+  }
+
+  @Get('/:id')
+  getTaskById(@Param('id') id: string): Task {
+    return this.tasksService.getTaskById(id);
+  }
+
+  @Post()
+  createTask(@Body() createTaskDto: CreateTaskDto): Task {
+    return this.tasksService.createTask(createTaskDto);
   }
 }
